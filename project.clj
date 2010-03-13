a796f89875c73218260e822bd797a8b5e0b65914
Martin Clausen <martin.clausen@gmail.com>
2010-03-13 20:04:17 +0100

    Change clojure jar to 1.1.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-war-test "1.0.0-SNAPSHOT" 
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
 		 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
 		 [org.clojars.liebke/compojure "0.3.1-master"]]
   :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.1"]
